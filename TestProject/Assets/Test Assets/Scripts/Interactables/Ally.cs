4a17d35ad718cec55667264ba6d89790449ef1e8
Cody Jones <codyjamesjones@icloud.com>
2020-09-30 20:33:58 -0400

    Feature Addition
    Implemented basic dialogue system.

---
+++ TestProject/Assets/Test Assets/Scripts/Interactables/Ally.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ally : Interactable
+{
+    public Dialogue dialogue;
+
+    public override void Interact()
+    {
+        TriggerDialogue();
+    }
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+}
